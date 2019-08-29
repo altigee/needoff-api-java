d4f2ac2d0146a89451c6bd58f632e6543f500047
Vitalii <vtomkiv@gmail.com>
2019-08-29 13:36:31 +0300

    NPE safe auth facade implementation

--- src/main/java/com/altigee/needoff/auth/service/AuthenticationFacadeImpl.java
+++ src/main/java/com/altigee/needoff/auth/service/AuthenticationFacadeImpl.java
@@ -3,25 +3,23 @@ package com.altigee.needoff.auth.service;
 import com.altigee.needoff.auth.data.AccountRepo;
 import com.altigee.needoff.auth.model.Account;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
 @Component
-public class AuthenticationFacadeImpl  implements AuthenticationFacade{
+public class AuthenticationFacadeImpl implements AuthenticationFacade {
 
-    @Autowired
-    private AccountRepo accountRepo;
+  @Autowired private AccountRepo accountRepo;
 
-    @Override public Optional<Account> getAuthenticatedAccount() {
-
-       Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-       if (principal instanceof Long){
-           var accountId =  (long) principal;
-           return accountRepo.findById(accountId);
-       }
-
-       return Optional.empty();
-    }
+  @Override
+  public Optional<Account> getAuthenticatedAccount() {
+    return Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication())
+        .map(Authentication::getPrincipal)
+        .filter(principal -> principal instanceof Long)
+        .map(Long.class::cast)
+        .flatMap(accountRepo::findById);
+  }
 }
